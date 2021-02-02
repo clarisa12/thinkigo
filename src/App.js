d19e9052ceb3d82ebb05609af8d8c4b3f7624afe
clarisa12 <clarisa.abalasei@gmail.com>
2021-02-02 18:24:44 +0200

    add login and register page

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import Container from "./components/container/Container";
 import Navigation from "./components/Navigation";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
-  const [login, setLogin] = useState(false);
+    const [login, setLogin] = useState(false);
 
-  return (
-    <Router>
-      <div className="App">
-        {/* <Navigation /> */}
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/board" component={Container} />
-        </Switch>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                {/* <Navigation /> */}
+                <Switch>
+                    <Route path="/login" component={Login} />
+                    <Route path="/board" component={Container} />
+                    <Route path="/register" component={Register} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
