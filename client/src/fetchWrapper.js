6df43f317723a0fe7d54ae039bcde4bb92f38309
clarisa12 <clarisa.abalasei@gmail.com>
2021-02-09 23:51:01 +0200

    add auth headers on the api fetch

---
+++ client/src/fetchWrapper.js
@@ -0,0 +1,24 @@
+import { getApiPath } from "./utils";
+import AuthService from "./AuthService";
+
+export const fetchWrapper = (path, method = "GET", body) => {
+    let headers = {
+        "Content-Type": "application/json",
+    };
+
+    const authHeader = AuthService.getAuthorizationHeader();
+    if (authHeader) headers = { ...headers, ...authHeader };
+    let options = {
+        method,
+        withCredentials: true,
+        headers,
+    };
+
+    if (body) {
+        options = { ...options, body: JSON.stringify(body) };
+    }
+    console.log(options);
+    fetch(getApiPath(path), options)
+        .then((res) => res.json())
+        .catch((error) => ({ success: false, message: error }));
+};
