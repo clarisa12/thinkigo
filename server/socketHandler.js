f9966e9bc2ddc7157553871361a28c1ce8d51c4e
clarisa12 <clarisa.abalasei@gmail.com>
2021-02-16 01:06:02 +0200

    add mongodb logic and cleanup

--- server/socketHandler.js
+++ server/socketHandler.js
@@ -1,7 +1,16 @@
-import { flushBoardData2Mongo } from "./controllers/boardController.js";
+import {
+    flushBoardData2Mongo,
+    getBoardData,
+} from "./controllers/boardController.js";
 import { Server } from "socket.io";
 
 import redis from "./redis.js";
+
+const logInfo = (socket, room, db) => {
+    console.info(
+        `Client ${socket.id} joined room ${room}\nUsing: ${db.toUpperCase()}`
+    );
+};
 
 // Keep track of # connected clients
 // in order to flush the data from redis into mongo-db
@@ -26,19 +35,25 @@ export default function io(server) {
         // ==================
         // Handlers
         function joinRoomHandler(room) {
-            console.info(`Client ${socket.id} joined room ${room}`);
-
             socket.join(room);
             // Attach room id to socket object
             socket.room = room;
             connectedUsers.set(room, connectedUsers.get(room) + 1 || 1);
 
             // Whenever a new client connects check if there is data on redis
-
             if (connectedUsers.get(room) === 1) {
                 // load from mongo database
+                getBoardData(room).then((board) => {
+                    logInfo(socket, room, "mongo");
+                    // load data in memory on redis
+                    if (board.data) {
+                        redis.store(room, board.data);
+                        socket.emit("draw", JSON.parse(board.data));
+                    }
+                });
             } else {
-                // try redis
+                logInfo(socket, room, "redis");
+                // try to get from redis
                 redis.retrieve(room, (err, data) => {
                     if (data) {
                         socket.emit("draw", JSON.parse(data));
@@ -61,8 +76,11 @@ export default function io(server) {
             const { room } = socket;
             connectedUsers.set(room, connectedUsers.get(room) - 1);
             if (connectedUsers.get(room) === 0) {
-                //TODO: Flush redis data to mongo
-                flushBoardData2Mongo(socket.room);
+                redis.retrieve(socket.room, (err, data) => {
+                    // cleanup memory associated with room
+                    redis.delete(socket.room);
+                    flushBoardData2Mongo(socket.room, data);
+                });
             }
         }
     }
