7022db5b4fca91e782073d9a1a16731295f466c2
clarisa12 <clarisa.abalasei@gmail.com>
2021-02-16 01:00:38 +0200

    fix fetchWrapper

--- client/src/fetchWrapper.js
+++ client/src/fetchWrapper.js
@@ -2,23 +2,22 @@ import { getApiPath } from "./utils";
 import AuthService from "./AuthService";
 
 export const fetchWrapper = (path, method = "GET", body) => {
-  let headers = {
-    "Content-Type": "application/json",
-  };
+    let headers = {
+        "Content-Type": "application/json",
+    };
 
-  const authHeader = AuthService.getAuthorizationHeader();
-  if (authHeader) headers = { ...headers, ...authHeader };
-  let options = {
-    method,
-    withCredentials: true,
-    headers,
-  };
+    const authHeader = AuthService.getAuthorizationHeader();
+    if (authHeader) headers = { ...headers, ...authHeader };
+    let options = {
+        method,
+        withCredentials: true,
+        headers,
+    };
 
-  if (body) {
-    options = { ...options, body: JSON.stringify(body) };
-  }
-  console.log(options);
-  fetch(getApiPath(path), options)
-    .then((res) => res.json())
-    .catch((error) => ({ success: false, message: error }));
+    if (body) {
+        options = { ...options, body: JSON.stringify(body) };
+    }
+    return fetch(getApiPath(path), options)
+        .then((res) => res.json())
+        .catch((error) => ({ success: false, message: error }));
 };
