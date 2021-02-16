d993ec5f52f026df406125312b11ac09ec746e2d
clarisa12 <clarisa.abalasei@gmail.com>
2021-02-17 00:48:11 +0200

    Add Notfound component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
 import "./App.css";
 import Container from "./components/Container/Container";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import {
+    Route,
+    BrowserRouter as Router,
+    Switch,
+    Redirect,
+} from "react-router-dom";
 import Register from "./components/Login/Register";
 import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound";
+
 import Login from "./components/Login/Login";
 import { DataProvider } from "./DataContext";
-
 function App() {
-  return (
-    <DataProvider>
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route path="/board/:id" component={Container} />
-            <Route path="/register" component={Register} />
-          </Switch>
-        </div>
-      </Router>
-    </DataProvider>
-  );
+    return (
+        <DataProvider>
+            <Router>
+                <div className="App">
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/login" component={Login} />
+                        <Route path="/board/:id" component={Container} exact />
+                        <Route path="/register" component={Register} />
+                        <Route path="/404" component={NotFound} />
+                        <Redirect from="*" to="/404" />
+                    </Switch>
+                </div>
+            </Router>
+        </DataProvider>
+    );
 }
 
 export default App;
---
+++ client/src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotFound() {
+    return (
+        <div>
+            <img src="https://i.pinimg.com/originals/86/41/80/86418032b715698a4dfa6684b50c12af.gif"></img>
+        </div>
+    );
+}
